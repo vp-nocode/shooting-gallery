d0f4496f57da5026449eac2b448b10fd0da8863a
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-04-13 00:06:46 +0500

    add const

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('game "Shooting gallery"')
+
+icon = pygame.image.load("")
+
+
+
 runnig = True
 while runnig:
     pass
