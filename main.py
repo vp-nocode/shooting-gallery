4a552c72fe4ea86e9eebe44a1f33fc25357b0b31
vp-nocode <166547991+vp-nocode@users.noreply.github.com>
2024-04-13 01:12:24 +0500

    add new target

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ icon = pygame.image.load("img/shooter.jpg")
 pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/target.png")
-target_width = 50
-target_height = 50
+target_width = 80
+target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
